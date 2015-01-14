d824948ffb348e95f4947477722df10790683263
henry <henry@postb.us>
2015-01-14 09:32:51 +0100

    refactor of database

--- app/database.php
+++ app/database.php
@@ -9,22 +9,14 @@
  */
 function db_create($table, array $data)
 {
-    list($fileHandler, $content) = db_get_file($table);
+    // get the file handler and contents of the file
+    list($fileHandler, $contents) = db_get_file($table);
 
     // add the new data to the end of the array
-    array_push($content, $data);
+    $contents[] = $data;
 
-    // encode the array to a string
-    $content = json_encode($content);
-
-    // write the new content to the file
-    $success = fwrite($fileHandler, $content);
-
-    // close the file handle
-    fclose($fileHandler);
-
-    // return true if success contains other than boolean false
-    return $success !== false;
+    // save the data to the database
+    return db_save($fileHandler, $contents);
 }
 
 /**
@@ -39,14 +31,84 @@ function db_get_file($table)
     // set the complete path and filename
     $fileName = __DIR__ . "/db/{$table}.json";
 
-    // open the file for reading and writing
-    $fileHandler = fopen($fileName, "w");
+    // get the contents of a file
+    $contents = @file_get_contents($fileName);
 
-    // get the content of the file
-    $content = fgets($fileHandler);
+    // open the file for reading and writing
+    $fileHandler = fopen($fileName, "w+");
 
     // decode to array if not empty else new array
-    $content = ($content != "") ? json_decode($content, true) : [];
+    $contents = $contents ? json_decode($contents, true) : [];
 
-    return [$fileHandler, $content];
+    // return the file handler and contents of the file
+    return [$fileHandler, $contents];
+}
+
+/**
+ * Save the file and return a boolean if successful
+ *
+ * @param object $fileHandler
+ * @param array $contents
+ * @return bool
+ */
+function db_save($fileHandler, array $contents)
+{
+    // add index
+    $contents = db_index($contents);
+
+    // encode the array to a string
+    $contents = json_encode($contents);
+
+    // write the new content to the file
+    $success = fwrite($fileHandler, $contents);
+
+    // close the file handle
+    fclose($fileHandler);
+
+    // return the result
+    return $success  !== false;
+}
+
+/**
+* Add the index to the $content array items
+*
+* @param array $contents
+* @return array
+*/
+function db_index($contents)
+{
+    // get the last item
+    $last_item = end($contents);
+
+    // check if the last item has an id
+    if (!isset($last_item['id']))
+    {
+        // set the default id
+        $item_id = 1;
+
+        // check if there are more than 1 items
+        if (count($contents) > 1)
+        {
+            // get the former last item key
+            $former_last_array_key = (count($contents) - 2);
+
+            // check if it exists
+            if (array_key_exists($former_last_array_key, $contents))
+            {
+                // and add 1
+                $item_id = $contents[$former_last_array_key]['id'] + 1;
+            }
+        }
+
+        // get the last item array key
+        $last_item_array_key = (count($contents) - 1);
+
+        // set the new id to the last item if it exists in the array
+        if (array_key_exists($last_item_array_key, $contents))
+        {
+            $contents[$last_item_array_key]['id'] = $item_id;
+        }
+    }
+
+    return $contents;
 }
